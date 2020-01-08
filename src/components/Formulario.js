606bf3186c633b763b9e178114c89ab0c24f3a80
Eder Ramirez <eder.ramirez87@gmail.com>
2020-01-08 17:44:13 -0600

    Displaying cryptocurrencies in select

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Criptomoneda from './Criptomoneda';
 
 const Formulario = () => {
+  const [ criptomonedas, guardarCriptomonedas ] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=15&tsym=USD"
+      const resultado = await Axios.get(url)
+      console.log("TCL: consultarAPI -> resultado", resultado.data.Data)
+      guardarCriptomonedas(resultado.data.Data)
+    }
+
+    consultarAPI()
+
+    //Colocar respuesta en el staet
+  }, [])
+
   return (
     <form>
       <div className="row">
@@ -15,7 +32,15 @@ const Formulario = () => {
       </div>
       <div className="row">
         <label htmlFor="">Elige tu Criptomoneda</label>
-        <select className="u-full-width"></select>
+        <select className="u-full-width">
+          {
+            criptomonedas.map(criptomoneda => (
+              <Criptomoneda 
+                key={criptomoneda.CoinInfo.Id}
+                criptomoneda={criptomoneda} />
+            ))
+          }
+        </select>
       </div>
     </form>
   );
