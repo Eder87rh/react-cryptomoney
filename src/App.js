d87015e62c9a9faa7e401abb4dfac7b5870e0344
Eder Ramirez <eder.ramirez87@gmail.com>
2020-01-09 11:01:31 -0600

    Spinner

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import imagen from './cryptomonedas.png'
 import Formulario from './components/Formulario';
+import Spinner from './components/Spinner';
 import Axios from 'axios';
 
 function App() {
   const [moneda, guardarMoneda] = useState('');
   const [criptomoneda, guardarCriptomoneda] = useState('');
+  const [cargando, guardarCargando] = useState(false);
 
   useEffect(() => {
     const cotizarCriptomoneda = async () => {
+      if(moneda === '') return;
+
+      guardarCargando(true);
       const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
       const resultado = await Axios.get(url);
       console.log("TCL: cotizarCriptomoneda -> resultado", resultado)
+
+      setTimeout(() => {
+        guardarCargando(false);
+      }, 3000);
       
     }
 
     cotizarCriptomoneda()
   }, [criptomoneda, moneda]);
+
+  // Mostrar Spinner o resultado
+  const componente = cargando ? <Spinner/> : null;
 
   return (
     <div className="container">
@@ -30,6 +42,7 @@ function App() {
             guardarMoneda={guardarMoneda}
             guardarCriptomoneda={guardarCriptomoneda}
           />
+          {componente}
         </div>
       </div>
     </div>
